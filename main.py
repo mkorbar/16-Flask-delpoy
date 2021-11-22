ba7ef9cb1ed4d8df025cacd1d4c2bb9711029a32
Matej Korbar <matej.korbar@dlabs.si>
2021-11-22 18:40:14 +0100

    Add loggin screen, move secret_number to database

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import os
 import random
-
-from flask import Flask, render_template, request, make_response
+import hashlib
+from flask import Flask, render_template, request, make_response, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite')
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    name = db.Column(db.String, unique=False)
+    password = db.Column(db.String, unique=False)
+    secret_num = db.Column(db.Integer, unique=False)
+
+
+db.create_all()
 
 @app.route("/")
 def index():
@@ -35,24 +51,67 @@ def portfolio():
     return render_template('portfolio.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        # če uporabnik ne obstaja, ustvarimo novega in ga shranimo v bazo
+        if not user:
+            # v bazi shranjujemo hashed-gesla, ne clear-text
+            hashed_pass = hashlib.sha256(password.encode()).hexdigest()
+            user = User(email=email, name=name, password=hashed_pass)
+            db.session.add(user)
+            db.session.commit()
+        else:
+            # ali se geslo iz baze ujema z geslom iz prijavnega obrazca
+            if user.password != hashlib.sha256(password.encode()).hexdigest():
+                return render_template('index.html', message='Error logging you in, wrong password')
+            # ali se uporabnisko ime iz baze ujema z uporabniskim imenom iz prijavnega obrazca
+            if user.name != name:
+                return render_template('index.html', message='Error logging you in, wrong user name')
+
+        response = make_response(render_template('index.html', message='You were successfully logged in'))
+        response.set_cookie('user_email', email)
+        return response
+
+
 @app.route('/game', methods=['GET', 'POST'])
 def guessing_game():
+
+    user_email = request.cookies.get('user_email')
+    if not user_email:
+        return redirect('/login')
+    else:
+        user = User.query.filter_by(email=user_email).first()
+
     if request.method == 'GET':
-        secret_number = request.cookies.get('secret_number')
+        secret_number = user.secret_num
         response = make_response(render_template('game.html'))
         if not secret_number:
             secret_number = random.randint(1, 30)
-            response.set_cookie('secret_number', str(secret_number))
+            user.secret_num = secret_number
+            db.session.add(user)
+            db.session.commit()
         return response
     elif request.method == 'POST':
 
         guess = int(request.form.get("numberGuess"))
-        secret_number = int(request.cookies.get("secret_number"))
+        secret_number = user.secret_num
 
         if guess == secret_number:
             message = "Correct! The secret number is {0}".format(str(secret_number))
             response = make_response(render_template("game.html", message=message, err=False))
-            response.set_cookie("secret_number", str(random.randint(1, 30)))  # set the new secret number
+            # save new secret number to db
+            user.secret_num = random.randint(1, 30)
+            db.session.add(user)
+            db.session.commit()
             return response
         elif guess > secret_number:
             message = "Your guess is not correct... try something smaller."
