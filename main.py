7c159a4a9459c0a4301c8cdcf321c9380a34bb8a
Matej Korbar <matej.korbar@dlabs.si>
2021-11-22 17:10:37 +0100

    Add a Guess the secret number game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
@@ -28,10 +30,36 @@ def oMeni():
         return response
 
 
-
 @app.route("/portfolio")
 def portfolio():
     return render_template('portfolio.html')
+
+
+@app.route('/game', methods=['GET', 'POST'])
+def guessing_game():
+    if request.method == 'GET':
+        secret_number = request.cookies.get('secret_number')
+        response = make_response(render_template('game.html'))
+        if not secret_number:
+            secret_number = random.randint(1, 30)
+            response.set_cookie('secret_number', str(secret_number))
+        return response
+    elif request.method == 'POST':
+
+        guess = int(request.form.get("numberGuess"))
+        secret_number = int(request.cookies.get("secret_number"))
+
+        if guess == secret_number:
+            message = "Correct! The secret number is {0}".format(str(secret_number))
+            response = make_response(render_template("game.html", message=message, err=False))
+            response.set_cookie("secret_number", str(random.randint(1, 30)))  # set the new secret number
+            return response
+        elif guess > secret_number:
+            message = "Your guess is not correct... try something smaller."
+            return render_template("game.html", message=message, err=True)
+        elif guess < secret_number:
+            message = "Your guess is not correct... try something bigger."
+            return render_template("game.html", message=message, err=True)
 
 
 if __name__ == '__main__':
